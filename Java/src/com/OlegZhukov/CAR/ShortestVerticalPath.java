40c3c69e5d57f1f9b20493b3541a32402198559b
OlegZhukov <unoccupied.yet@gmail.com>
2014-05-31 17:17:11 +1200

    Added copyright header to all .java files

--- Java/src/com/OlegZhukov/CAR/ShortestVerticalPath.java
+++ Java/src/com/OlegZhukov/CAR/ShortestVerticalPath.java
@@ -1,3 +1,10 @@
+/*
+ * Content-Aware Resizing Tool.
+ *
+ * Copyright © 2014, Oleg Zhukov (mailto:mail@OlegZhukov.com)
+ * 
+ * This software is licensed under GPL 3.0 license.
+ */
 package com.OlegZhukov.CAR;
 
 class ShortestVerticalPath {
