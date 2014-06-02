3a11b8b413dc31d4ab6fd29226699d5300df8078
OlegZhukov <unoccupied.yet@gmail.com>
2014-06-02 12:40:25 +1200

    - Ported everything from Java - Calculating energy in the simplest way (just constant value)

---
+++ CS/CAR.Test/FindingAndRemovingSeamsSteps.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OlegZhukov.CAR;
+using System.IO;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace CAR.Test
+{
+    [Binding]
+    public class FindingAndRemovingSeamsSteps
+    {
+        String inputImageFile;
+        String energyFunc;
+
+        [BeforeFeature]
+        public static void BeforeFeature()
+        {
+            Directory.SetCurrentDirectory("..\\..\\..\\..\\Features");
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            energyFunc = "";
+        }
+
+        [Given(@"image (.*)")]
+        public void GivenImage(String inputImageFile)
+        {
+            this.inputImageFile = inputImageFile;
+        }
+
+        [When(@"I use the energy function (.*)")]
+        public void UseEnergyFunc(String energyFunc)
+        {
+            this.energyFunc = energyFunc;
+        }
+
+        [When(@"I resize the image to (.*) width and (.*) height")]
+        public void resize(String v, String h)
+        {
+            if (energyFunc != "") energyFunc = " -e=" + energyFunc;
+            Program.Main(String.Format("-w={0} -h={1}{2} {3}", v, h, energyFunc,
+                    inputImageFile).Split(' '));
+        }
+
+        [Then(@"I should get image (.*)")]
+        public void shouldGetImage(String expectedImageFile)
+        {
+            using (Bitmap expectedImage = new Bitmap(expectedImageFile),
+                   actualImage = new Bitmap(Program.getOutputPictureFile()))
+            {
+                Assert.AreEqual(expectedImage.Width, actualImage.Width);
+                Assert.AreEqual(expectedImage.Height, actualImage.Height);
+                for (int x = 0; x < expectedImage.Width; x++)
+                    for (int y = 0; y < actualImage.Height; y++)
+                        if (expectedImage.GetPixel(x, y) != actualImage.GetPixel(x, y))
+                            Assert.Fail();
+                Assert.IsTrue(true);
+            }
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            if (File.Exists(Program.getOutputPictureFile()))
+                File.Delete(Program.getOutputPictureFile());
+        }
+    }
+}
---
+++ CS/CAR/RGBGradientNormWithConstantBorders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace OlegZhukov.CAR
+{
+    class RGBGradientNormWithConstantBorders : EnergyFunction
+    {
+        const int BORDER_ENERGY = 195075;
+
+        public override float value(int i, int x, int y)
+        {
+            if (x == 0 || x == n - 1 || y == 0 || y == m - 1) return BORDER_ENERGY;
+
+            Color upper = linearizedPicture[i - n], lower = linearizedPicture[i + n],
+                  left = linearizedPicture[i - 1], right = linearizedPicture[i + 1];
+            float rDeltaX = right.R - left.R, rDeltaY =
+                    lower.R - upper.R, gDeltaX =
+                    right.G - left.G, gDeltaY =
+                    lower.G - upper.G, bDeltaX =
+                    right.B - left.B, bDeltaY =
+                    lower.B - upper.B;
+            return (float)Math.Sqrt(rDeltaX * rDeltaX + rDeltaY * rDeltaY
+                    + gDeltaX * gDeltaX + gDeltaY * gDeltaY
+                    + bDeltaX * bDeltaX + bDeltaY * bDeltaY);
+        }
+    }
+}
