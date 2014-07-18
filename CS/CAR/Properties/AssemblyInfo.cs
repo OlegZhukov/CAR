815b4b6066df92687b8dec01854b821617522a13
OlegZhukov <unoccupied.yet@gmail.com>
2014-07-18 21:35:08 +0400

    Minor change to AssemblyInfo.cs

--- CS/CAR/Properties/AssemblyInfo.cs
+++ CS/CAR/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CAR")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Oleg Zhukov 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
